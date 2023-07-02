ecc8c1b848b0ff25ba3390e7f8d7a85bff51a042
GodnessCook <137362368+GodnessCook@users.noreply.github.com>
2023-07-02 17:03:18 +0500

    Update dateBase.ts

--- src/utils/dateBase.ts
+++ src/utils/dateBase.ts
@@ -10,39 +10,39 @@ export const ServicesData : {
 }[] = [
     {
         id: 1,
-        name: "UX / UI Design",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Mobile app development",
+        text: "We can create a customized e-commerce solution that fits your business needs and helps you increase your online sales.",
         icon: UiIcon
     },
         
     {
         id: 2,
-        name: "UX / UI Design",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Content Management System (CMS) Development",
+        text: "The process, best practices and technologies used are both proven and practical.",
         icon: UiIcon
     },
     {
         id: 3,
-        name: "UX / UI Design",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "UI/UX Design",
+        text: "We focus on creating intuitive and visually appealing user interfaces (UI) and user experiences (UX) to ensure your website is easy to use and engages your audience.",
         icon: UiIcon
     },
     {
         id: 4,
-        name: "UX / UI Design",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Website Maintenance and Support",
+        text: "We provide ongoing maintenance and support services to ensure your website stays up to date and secure, giving you peace of mind.",
         icon: UiIcon
     },
     {
         id: 5,
-        name: "UX / UI Design",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Custom Software Development",
+        text: "We can build customized software solutions that address your unique business needs and help you streamline your operations.",
         icon: UiIcon
     },
     {
         id: 6,
-        name: "UX / UI Design",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Search Engine Marketing (SEM)",
+        text: "Our SEM services include pay-per-click advertising campaigns and remarketing strategies to drive more traffic and conversions to your website.",
         icon: UiIcon
     },
     ]
@@ -55,39 +55,45 @@ export const AdventagesData : {
 }[] = [
     {
         id: 1,
-        name: "QUALITY",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Streamlined Design Process",
+        text: "Experience a seamless website creation journey with our streamlined design process. Our platform is tailored to ensure that you can effortlessly bring your vision to life, no matter your skill level. 
+            We are always eager to hear your thoughts and ideas. Share your feedback and suggestions with us, and together we can create the perfect website for you.",
         icon: Adventage
     },
         
     {
         id: 2,
-        name: "QUALITY",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Personalized Customization",
+        text: "Craft a distinctive online presence that reflects your unique style and personality. Our customizable templates and user-friendly tools empower you to create a website that showcases your individuality without any technical barriers.
+            Take advantage of our versatile design features and make your website stand out from the crowd.",
         icon: Adventage
     },
     {
         id: 3,
-        name: "QUALITY",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Eco-conscious Approach",
+        text: "Our team is dedicated to making a positive impact on the environment by minimizing our carbon footprint and promoting sustainable practices.
+            By choosing to build your website with us, you are directly contributing to a greener future for our planet.",
         icon: Adventage
     },
     {
         id: 4,
-        name: "QUALITY",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Mobile-Friendly Design",
+        text: "In today's digital landscape, mobile compatibility is key. Our platform ensures that your website looks great and functions smoothly on all devices, providing an optimal browsing experience for your visitors.
+            Stay ahead of the curve with our mobile-responsive designs and tools.",
         icon: Adventage
     },
     {
         id: 5,
-        name: "QUALITY",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Robust Data Protection",
+        text: "We take the security of your website and the privacy of your visitors very seriously. With over a decade of experience in the industry, we provide a reliable and secure solution that keeps your data safe.
+            Rest easy knowing that your website and its data are protected by our state-of-the-art security measures.",
         icon: Adventage
     },
     {
         id: 6,
-        name: "QUALITY",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        name: "Attentive Customer Support",
+        text: "Our dedicated customer support team is here to help you every step of the way. We believe in providing personalized, human assistance to ensure your website's success.
+            When you create a website with us, you can count on our friendly and knowledgeable staff to guide you through the process and address any concerns you may have.",
         icon: Adventage
     },
     ]
