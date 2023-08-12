34ea6a3a0faa35a4b01f5696187c18034bc39fc0
Nurulloh <nurulloh1809@gmail.com>
2023-08-12 17:56:41 +0500

    rate card changed

--- src/components/OurProcess/OurProcess.tsx
+++ src/components/OurProcess/OurProcess.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react'
-import { OurProcessData, ReviewsData } from '../../utils/dateBase'
-import ReviewsCard from '../ReviewCard/ReviewsCard'
-import Text from '../Text/Text'
-import styles from "./OurProcess.module.css"
-import _ from 'lodash'
-import { COLORS } from '../../utils/color'
+import React, { useEffect } from "react";
+import { OurProcessData, ReviewsData } from "../../utils/dateBase";
+import ReviewsCard from "../ReviewCard/ReviewsCard";
+import Text from "../Text/Text";
+import styles from "./OurProcess.module.css";
+import _ from "lodash";
+import { COLORS } from "../../utils/color";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -15,12 +15,23 @@ const OurProcess = () => {
     }, []);
     return (
         <div className={styles.container}>
-            <div className={styles.header} data-aos="fade-up"
-                data-aos-duration="1500">
-                <Text textAlign={"center"} text='OUR PROCESS' family="BenzinBold" textSize='thirtySix' />
+            <div
+                className={styles.header}
+                data-aos="fade-up"
+                data-aos-duration="1500"
+            >
+                <Text
+                    textAlign={"center"}
+                    text="OUR PROCESS"
+                    family="BenzinBold"
+                    textSize="thirtySix"
+                />
             </div>
-            <div className={styles.content} data-aos="fade-up"
-                data-aos-duration="1500">
+            <div
+                className={styles.content}
+                data-aos="fade-up"
+                data-aos-duration="1500"
+            >
                 <div className={styles.cardsBox}>
                     <div className={styles.line}></div>
                     {OurProcessData.map((e, index) => {
@@ -28,15 +39,21 @@ const OurProcess = () => {
                             <div key={index} className={styles.cards}>
                                 <div className={styles.dot}></div>
                                 <h2 className={styles.Count}>{e.id}</h2>
-                                <Text text={e.title} textSize={'twenty'} />
-                                <Text family='FuturaBook' textAlign={"center"} text={e.text} textSize={'fourteen'} color={COLORS.grey} />
+                                <Text text={e.title} textSize={"twenty"} />
+                                <Text
+                                    family="FuturaBook"
+                                    textAlign={"center"}
+                                    text={e.text}
+                                    textSize={"fourteen"}
+                                    color={COLORS.grey}
+                                />
                             </div>
-                        )
+                        );
                     })}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default OurProcess
+export default OurProcess;
--- src/screens/developers/Developers.tsx
+++ src/screens/developers/Developers.tsx
@@ -1,16 +1,17 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import DevelopersCard from '../../components/DevelopersCard/DevelopersCard'
-import Header from '../../components/Header/Header'
-import Text from '../../components/Text/Text'
-import useRootStore from '../../Hooks/useRootStore'
-import { ReviewsData } from '../../utils/dateBase'
-import styles from "./Developers.module.css"
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import DevelopersCard from "../../components/DevelopersCard/DevelopersCard";
+import Header from "../../components/Header/Header";
+import Text from "../../components/Text/Text";
+import useRootStore from "../../Hooks/useRootStore";
+import { ReviewsData } from "../../utils/dateBase";
+import styles from "./Developers.module.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { ASSETS } from '../../utils/assetsRequires'
-import ViewProfile from '../../components/ViewProfile/ViewProfile'
-import WriteToDeveloper from '../../components/WriteToDeveloper/WriteToDeveloper'
+import { ASSETS } from "../../utils/assetsRequires";
+import ViewProfile from "../../components/ViewProfile/ViewProfile";
+import WriteToDeveloper from "../../components/WriteToDeveloper/WriteToDeveloper";
+import Footer from "../../components/Footer/Footer";
 
 const Developers = () => {
     useEffect(() => {
@@ -19,24 +20,35 @@ const Developers = () => {
     }, []);
     const { show, visiable } = useRootStore().visibleStore;
     const { getUserData, userData } = useRootStore().userStore;
-    const router = useNavigate()
+    const router = useNavigate();
     const Click = (id: number) => {
         show("viewProfile");
         getUserData(id);
-        router(`?programmer/${id}`)
+        router(`?programmer/${id}`);
         if (visiable.viewProfile === true)
-            document.body.style.overflow = "hidden"
+            document.body.style.overflow = "hidden";
     };
     return (
         <>
             <Header />
             <div className={styles.container}>
-                <div className={styles.top} data-aos="fade-up"
-                    data-aos-duration="1500">
-                    <Text text="Developers for your business" textAlign={"flex-start"} family="BenzinBold" textSize="thirtySix" />
+                <div
+                    className={styles.top}
+                    data-aos="fade-up"
+                    data-aos-duration="1500"
+                >
+                    <Text
+                        text="Developers for your business"
+                        textAlign={"flex-start"}
+                        family="BenzinBold"
+                        textSize="thirtySix"
+                    />
                 </div>
-                <div className={styles.cardsBox} data-aos="fade-up"
-                    data-aos-duration="1500">
+                <div
+                    className={styles.cardsBox}
+                    data-aos="fade-up"
+                    data-aos-duration="1500"
+                >
                     {ReviewsData.map((e, index) => {
                         return (
                             <DevelopersCard
@@ -50,13 +62,25 @@ const Developers = () => {
                         );
                     })}
                 </div>
-                <img data-aos="fade-down-right" data-aos-duration="1200" className={styles.backOne} src={ASSETS.back3} alt="back" />
-                <img data-aos="fade-down-left" data-aos-duration="1200" className={styles.backTwo} src={ASSETS.back4} alt="back" />
+                <img
+                    data-aos="fade-down-right"
+                    data-aos-duration="1200"
+                    className={styles.backOne}
+                    src={ASSETS.back3}
+                    alt="back"
+                />
+                <img
+                    data-aos="fade-down-left"
+                    data-aos-duration="1200"
+                    className={styles.backTwo}
+                    src={ASSETS.back4}
+                    alt="back"
+                />
             </div>
             <ViewProfile />
             <WriteToDeveloper />
         </>
-    )
-}
+    );
+};
 
-export default Developers
+export default Developers;
