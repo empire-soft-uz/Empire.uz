54b8a9b8bda458232c6d990f5f09c66cdd1f9442
Nurulloh <nurulloh1809@gmail.com>
2023-08-22 13:29:01 +0500

    Unnecessary package la has been removed

--- src/components/MainBack/MainBack.tsx
+++ src/components/MainBack/MainBack.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect } from "react";
-import { ArrowFall } from "../../assets/icons/Icons";
 import { ASSETS } from "../../utils/assetsRequires";
 import Header from "../Header/Header";
 import Text from "../Text/Text";
 import styles from "./MainBack.module.css";
-import { HiOutlineArrowDown } from "react-icons/hi";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Button from "../Button/Button";
--- src/components/UpBtn/UpBtn.tsx
+++ src/components/UpBtn/UpBtn.tsx
@@ -1,53 +1,58 @@
-import { observer } from 'mobx-react-lite'
-import React, { useEffect } from 'react'
-import useRootStore from '../../Hooks/useRootStore'
+import { observer } from "mobx-react-lite";
+import React, { useEffect } from "react";
+import useRootStore from "../../Hooks/useRootStore";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { IoIosArrowUp } from "react-icons/io"
-import styled from 'styled-components'
+import { IoIosArrowUp } from "react-icons/io";
+import styled from "styled-components";
 
 const UpBtn = () => {
     useEffect(() => {
         AOS.init();
         AOS.refresh();
     }, []);
-    const { visiable } = useRootStore().visibleStore
+    const { visiable } = useRootStore().visibleStore;
     const EventScrollTop = () => {
-        window.scrollTo({ top: 0, behavior: "smooth" })
-    }
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
     return (
         <Contianer>
-            <button data-aos="fade-up"
-                data-aos-duration="1500" onClick={EventScrollTop} className='btn' style={{ display: visiable.upBtn ? "block" : "none" }}>
+            <button
+                data-aos="fade-up"
+                data-aos-duration="1500"
+                onClick={EventScrollTop}
+                className="btn"
+                style={{ display: visiable.upBtn ? "block" : "none" }}
+            >
                 <IoIosArrowUp size={24} />
             </button>
         </Contianer>
-    )
-}
+    );
+};
 
 const Contianer = styled.div`
-    .btn{
+    .btn {
         position: fixed;
-        bottom: 100px;
+        bottom: 30px;
         right: 20px;
         top: auto;
         left: auto;
         z-index: 5;
         width: 40px;
         height: 40px;
-        background-color: #167A60;
+        background-color: #167a60;
         border: none;
         color: #fff;
         border-radius: 50%;
         cursor: pointer;
     }
 
-    .btn:hover{
+    .btn:hover {
         transform: scale(1.1);
         transition: ease-in-out 0.25s;
         background-color: #fff !important;
-        color: #167A60 !important;
-    } 
-`
+        color: #167a60 !important;
+    }
+`;
 
-export default observer(UpBtn)
+export default observer(UpBtn);
