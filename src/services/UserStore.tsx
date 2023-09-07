58079be00718938d73239e285d3a4fafe5d1d6dd
Nurulloh <nurulloh1809@gmail.com>
2023-09-07 15:38:54 +0500

    added phone number mask

--- src/services/UserStore.tsx
+++ src/services/UserStore.tsx
@@ -1,37 +1,49 @@
 import { find } from "lodash";
 import { makeAutoObservable } from "mobx";
-import { ReviewDataInitial, ReviewDataType, ServiceSendDataInitial, ServiceSendDataType } from "../types/types";
+import {
+    CountryStateInitial,
+    CountryStateType,
+    ReviewDataInitial,
+    ReviewDataType,
+    ServiceSendDataInitial,
+    ServiceSendDataType,
+} from "../types/types";
 import { ReviewsData } from "../utils/dateBase";
 
 export class UserStore {
     currentId?: number;
 
     constructor() {
-        makeAutoObservable(this)
+        makeAutoObservable(this);
     }
 
-
-    userData: ReviewDataType = ReviewDataInitial
+    userData: ReviewDataType = ReviewDataInitial;
 
     onViewProfile = (item: ReviewDataType) => {
-        this.userData = item
-    }
+        this.userData = item;
+    };
 
-    serviceSendData: ServiceSendDataType = ServiceSendDataInitial
+    countryData: CountryStateType = CountryStateInitial;
+
+    setCountryData = (value: string, key: keyof CountryStateType) => {
+        this.countryData[key] = value as never;
+    };
+
+    serviceSendData: ServiceSendDataType = ServiceSendDataInitial;
 
     setServiceSendMessage = (value: string, key: keyof ServiceSendDataType) => {
-        this.serviceSendData[key] = value as never
-    }
+        this.serviceSendData[key] = value as never;
+    };
 
     clearServiceSendDevForm = () => {
-        this.serviceSendData = ServiceSendDataInitial
-    }
+        this.serviceSendData = ServiceSendDataInitial;
+    };
 
     ServiceSendMessage = (item: ServiceSendDataType) => {
-        this.serviceSendData = item
-    }
+        this.serviceSendData = item;
+    };
 
     getUserData = (id: number) => {
         this.userData = find(ReviewsData, { id }) as ReviewDataType;
-    }
+    };
 }
